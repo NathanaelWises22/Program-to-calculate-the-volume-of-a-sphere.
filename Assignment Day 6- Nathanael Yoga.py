903ae98c5174bcc4866de65f7bef72107a1b116e
NathanaelWises22 <92627169+NathanaelWises22@users.noreply.github.com>
2021-11-04 21:11:27 +0700

    Add files via upload

---
+++ Assignment Day 6- Nathanael Yoga.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pi = 3.14
+radius = input("Enter Radius: ")
+print("Radius: " + str(radius))
+
+r = float(radius)
+
+V= 4.0/3.0*pi* r**3
+print('The volume of the sphere is: ',V)
+
+
+# In[ ]:
+
+
+
+
